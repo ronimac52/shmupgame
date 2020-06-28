851f810ef47224de881f35c99086b63735ba37dd
Ronan <ronanmusic@btinternet.com>
2020-06-28 11:51:50 +0100

    created basic player sprite

--- shmupgame.py
+++ shmupgame.py
@@ -19,8 +19,24 @@ pygame.mixer.init()  # for sound
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Shmup")
 clock = pygame.time.Clock()
+# set up Player class
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)# Initiate sprite
+        self.image = pygame.Surface((50, 40))# plain rectangle for now
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()# get rect from Pygame
+        self.rect.centerx = WIDTH / 2# position middle of window
+        self.rect.bottom = HEIGHT - 10# 10 px from bottom
+        self.speedx = 0 # To move side to side (x axis)
 
+    def update(self):# what happens every update in animation loop
+        self.rect.x += self.speedx # move on x axis by whatever speed is set on self.speed.x
+
+# spawn sprites
 all_sprites = pygame.sprite.Group()
+player = Player() #create new object in Player class
+all_sprites.add(player) # add object to all_sprites so it gets updated and drawn
 
 # Game Loop
 running = True
