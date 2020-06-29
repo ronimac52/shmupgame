c93d680ec58c435cc043603392ae171adea054e9
Ronan <ronanmusic@btinternet.com>
2020-06-29 10:49:25 +0100

    added rest of images and removed black fill around them (not blit BLACK)

--- shmupgame.py
+++ shmupgame.py
@@ -29,6 +29,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)# Initiate sprite
         self.image = pygame.transform.scale(player_img, (50, 38)) #pygame.transform.scale()changed(reduced) size
+        self.image.set_colorkey(BLACK) #made black around image transparent (not be blit)
         self.rect = self.image.get_rect()# get rect from Pygame
         self.rect.centerx = WIDTH / 2# position middle of window
         self.rect.bottom = HEIGHT - 10# 10 px from bottom
@@ -55,8 +56,8 @@ class Player(pygame.sprite.Sprite):
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30, 40))
-        self.image.fill(RED)
+        self.image = meteor_img
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(WIDTH - self.rect.width) # sprites appear randomly across screen
         self.rect.y = random.randrange(-100, -40) #spawn randomly vertically (off-screen)
@@ -74,8 +75,8 @@ class Mob(pygame.sprite.Sprite):
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 20))
-        self.image.fill(YELLOW)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
